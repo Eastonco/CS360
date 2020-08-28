32b25166c7c39dc641fab20328ba50089c3afee5
Connor Easton <eastonco@icloud.com>
2020-08-27 21:56:42 -0700

    added stack printing

--- prelab1/t.c
+++ prelab1/t.c
@@ -15,8 +15,12 @@ int main(int argc, char *argv[ ], char *env[ ])
   printf("&argc=%x argv=%x env=%x\n", &argc, argv, env);
   printf("&a=%8x &b=%8x &c=%8x\n", &a, &b, &c);
 
-//(1). Write C code to print values of argc and argv[] entries
-  printf("argc=%x argv=%x\n", argc, &*argv);
+  //(1). Write C code to print values of argc and argv[] entries
+  printf("argc=%x\n", argc);
+
+  for(int i = 0; i < argc; i++){
+    printf("argv[%d]=%s\n", i, argv[i]);
+  }
 
   a=1; b=2; c=3;
   A(a,b);
@@ -41,6 +45,9 @@ int B(int x, int y)
   int g,h,i;
   printf("enter B\n");
   // write C code to PRINT ADDRESS OF g,h,i
+  printf("g=%x h=%x i=%x\n", &g, &h, &i);
+
+
   g=7; h=8; i=9;
   C(g,h);
   printf("exit B\n");
@@ -51,19 +58,40 @@ int C(int x, int y)
   int u, v, w, i, *p;
 
   printf("enter C\n");
-  // write C cdoe to PRINT ADDRESS OF u,v,w,i,p;
+  // write C code to PRINT ADDRESS OF u,v,w,i,p;
+  printf("u=%x v=%x w=%x i=%x &p=%x\n\n", &u, &v, &w, &i, p);
+
+
   u=10; v=11; w=12; i=13;
 
   FP = (int *)getebp();  // FP = stack frame pointer of the C() function
+  printf("FP = ebp = %x\n", FP);
 
 
 //(2). Write C code to print the stack frame link list.
+  p = (int *)FP;
+  printf("stack frame link list:\n");
+  while(p){
+    printf("%x -> ", p);
+    p = *p;
+  }
+  printf("%x\n", (void *)p);
 
 
- p = (int *)&p;
 
 //(3). Print the stack contents from p to the frame of main()
 //     YOU MAY JUST PRINT 128 entries of the stack contents.
+
+
+  printf("\n128 stack contents\n");
+
+  p = (int *)FP;
+  for(int i = 0; i < 128; i++){
+    printf("%x\t%x\n", p, *p);
+    p++;
+  }
+
+  //p = (int *)&p;
 
 //(4). On a hard copy of the print out, identify the stack contents
 //     as LOCAL VARIABLES, PARAMETERS, stack frame pointer of each function.
