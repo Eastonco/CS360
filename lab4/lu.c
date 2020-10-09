3360ba06f66f8a7bd508a4f4b0d040c2008491c8
Connor Easton <eastonco@icloud.com>
2020-10-09 13:12:48 -0700

    added lab 4

--- lab4/lu.c
+++ lab4/lu.c
@@ -4,13 +4,14 @@
 #include <math.h>
 #include <pthread.h>
 
-#define N 4
+#define N 8
 int n;
 
 double A[N][N], L[N][N], U[N][N];
 double B[N], b[N];
 int P[N];
 double Y[N], X[N];
+pthread_barrier_t barrier;
 
 int print(char c, double x[N][N])
 {
@@ -41,78 +42,115 @@ int printP()
 }
 
 // LU decomposition function
-int lu()
+int *lu(void *arg)
 {
     int i, j, k, m, itemp;
     double max;
     double temp;
     double si;
+    int myid = (int)arg;
 
     // LU decomposition loop
     for (k = 0; k < n; k++)
     {
-        max = 0;
-        printf("partial pivoting by row %d\n", k);
-        for (i = k; i < n; i++)
+        if (k == myid)
         {
-            if (max < fabs(A[i][k]))
-            { // partial pivoting
-                max = fabs(A[i][k]);
-                j = i;
+            max = 0;
+            printf("partial pivoting by row %d\n", k);
+            for (i = k; i < n; i++)
+            {
+
+                if (max < fabs(A[i][k]))
+                { // partial pivoting
+                    max = fabs(A[i][k]);
+                    j = i;
+                }
             }
         }
-        if (max == 0)
-        {
-            printf("zero pivot: singular A matrix\n");
-            exit(1);
-        }
+        pthread_barrier_wait(&barrier);
 
-        // swap P[k] and P[j];
-        itemp = P[k];
-        P[k] = P[j];
-        P[j] = itemp;
-
-        // swap row A[k] and row A[j]
-        printf("swap row %d and row %d of A\n", k, j);
-        for (m = 0; m < n; m++)
+        if (k == myid)
         {
-            temp = A[k][m];
-            A[k][m] = A[j][m];
-            A[j][m] = temp;
+            if (max == 0)
+            {
+                printf("zero pivot: singular A matrix\n");
+                exit(1);
+            }
         }
-        print('A', A);
-        getchar();
+        pthread_barrier_wait(&barrier);
+
+        if (k == myid)
+        {
+            // swap P[k] and P[j];
+            itemp = P[k];
+            P[k] = P[j];
+            P[j] = itemp;
+        }
+        pthread_barrier_wait(&barrier);
+
+        if (k == myid)
+        {
+            // swap row A[k] and row A[j]
+            printf("swap row %d and row %d of A\n", k, j);
+            for (m = 0; m < n; m++)
+            {
+                temp = A[k][m];
+                A[k][m] = A[j][m];
+                A[j][m] = temp;
+            }
+            print('A', A);
+            getchar();
+        }
+        pthread_barrier_wait(&barrier);
 
         //swap L[k][0,k-2] and L[j][0,k-2]
         for (m = 0; m < k - 2; m++)
         {
-            temp = L[k][m];
-            L[k][m] = L[j][m];
-            L[j][m] = temp;
-        }
-
-        // compute L U entries
-        U[k][k] = A[k][k];
-        for (i = k + 1; i < n; i++)
-        {
-            L[i][k] = A[i][k] / U[k][k];
-            U[k][i] = A[k][i];
-        }
-
-        // row reductions on A
-        printf("row reducations of A by row %d\n", k);
-        for (i = k + 1; i < n; i++)
-        {
-            for (m = k + 1; m < n; m++)
+            if (k == myid)
             {
-                A[i][m] -= L[i][k] * U[k][m];
+                temp = L[k][m];
+                L[k][m] = L[j][m];
+                L[j][m] = temp;
+            }
+            pthread_barrier_wait(&barrier);
+        }
+
+        if (k == myid)
+        {
+            // compute L U entries
+            U[k][k] = A[k][k];
+            for (i = k + 1; i < n; i++)
+            {
+                L[i][k] = A[i][k] / U[k][k];
+                U[k][i] = A[k][i];
             }
         }
-        print('A', A);
-        print('L', L);
-        print('U', U);
-        printP();
-        getchar();
+        pthread_barrier_wait(&barrier);
+
+        // row reductions on A
+        if (k == myid)
+        {
+            printf("row reducations of A by row %d\n", k);
+            for (i = k + 1; i < n; i++)
+            {
+                for (m = k + 1; m < n; m++)
+                {
+
+                    A[i][m] -= L[i][k] * U[k][m];
+                }
+            }
+        }
+        pthread_barrier_wait(&barrier);
+
+        if (k == myid)
+        {
+            print('A', A);
+            print('L', L);
+            print('U', U);
+            printP();
+            getchar();
+        }
+        pthread_barrier_wait(&barrier);
     }
 }
 
@@ -158,7 +196,21 @@ int main(int argc, char *argv[])
     printV('B', B);
     printP();
 
-    lu();
+    pthread_t threads[N];
+    pthread_barrier_init(&barrier, NULL, N); // set up barrier
+
+    printf("main: create N=%d working threads\n", N);
+
+    for (i = 0; i < N; i++)
+    {
+        pthread_create(&threads[i], NULL, lu, (void *)i);
+    }
+
+    printf("main: wait for all %d working threads to join\n", N);
+    for (i = 0; i < N; i++)
+    {
+        pthread_join(threads[i], NULL);
+    }
 
     // P L U are all computed; solve P*U*L*X = P*B
 
