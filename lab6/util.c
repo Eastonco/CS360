2279e49b5d18a1f426f569d5d3cb48889dd1b253
Connor Easton <eastonco@icloud.com>
2020-11-02 11:54:17 -0800

    added util functions from book

--- lab6/util.c
+++ lab6/util.c
@@ -66,20 +66,98 @@ MINODE *iget(int dev, int ino)
 void iput(MINODE *mip)
 {
   // dispose of minode pointed by mip
-  // Code in Chapter 11.7.2
+  INODE *ip;
+  int i, block, offset;
+  char buf[BLKSIZE];
+
+  if (mip == 0)
+    return;
+  mip->refCount--;       // dec refCount by 1
+  if (mip->refCount > 0) // still has user
+    return;
+  if (mip->dirty == 0) // no need to write back
+    return;
+
+  // write INODE back to disk
+  block = (mip->ino - 1) / 8 + iblock;
+  offset = (mip->ino - 1) % 8;
+
+  // get block containing this inode
+  get_block(mip->dev, block, buf);
+  ip = (INODE *)buf + offset;      // ip points at INODE
+  *ip = mip->INODE;                // copy INODE to inode in block
+  put_block(mip->dev, block, buf); // write back to disk
+  midalloc(mip);                   // mip->refCount = 0;
 }
 
 int search(MINODE *mip, char *name)
 {
   // search for name in (DIRECT) data blocks of mip->INODE
   // return its ino
-  // Code in Chapter 11.7.2
+
+  int i;
+  char *cp, temp[256], sbuf[BLKSIZE];
+  DIR *dp;
+  for (i = 0; i < 12; i++)
+  { //Search DIR direct blcoks only
+    if (mip->INODE.i_block[i] == 0)
+      return 0;
+    get_block(mip->dev, mip->INODE.i_block[i], sbuf);
+    dp = (DIR *)sbuf;
+    cp = sbuf;
+    while (cp < sbuf + BLKSIZE)
+    {
+      strncpy(temp, dp->name, dp->name_len);
+      temp[dp->name_len] = 0;
+      printf("%8d%8d%8u %s\n",
+             dp->inode, dp->rec_len, dp->name_len, temp);
+      if (strcmp(name, temp) == 0)
+      {
+        printf("found %s : inumber = %d\n", name, dp->inode);
+        return dp->inode;
+      }
+      cp += dp->rec_len;
+      dp = (DIR *)cp;
+    }
+  }
+  return 0;
 }
 
 int getino(char *pathname)
 {
   // return ino of pathname
-  // Code in Chapter 11.7.2
+  MINODE *mip;
+  int i, ino;
+  if (strcmp(pathname, "/") == 0)
+    return 2; // return root ino = 2
+  if (pathname[0] == "/")
+    mip = root; // if absolute pathname: start from root
+  else
+  {
+    mip = running->cwd // if relative pathname: start from cwd
+  }
+
+  mip->refCount++;
+
+  tokenize(pathname); // assume: name[], nname are globals
+
+  for(i =0; i <nname; i++){ //search for each component string
+    if(!S_ISDIR(mip->INODE.i_mode)){
+      printf("%s is not a directory\n", name[i]);
+      iput(mip);
+      return 0;
+    }
+    ino = search(mip, name[i]);
+    if(!ino){
+      printf("no such component name %s\n", name[i]);
+      iput(mip);
+      return 0;
+          }
+    iput(mip);
+    mip = iget(dev, ino);
+  }
+  iput(mip);
+  return ino;
 }
 
 int findmyname(MINODE *parent, u32 myino, char *myname)
