11d0ba024714a35f81ce6f7c91ac83d3bf0892ce
Connor Easton <eastonco@icloud.com>
2020-11-04 21:54:00 -0800

    commented type.h

--- lab6/mountroot/type.h
+++ lab6/mountroot/type.h
@@ -6,7 +6,7 @@
 #include <stdlib.h>
 #include <fcntl.h>
 #include <linux/fs.h>
-#include <ext2fs/ext2_fs.h> //install with 'sudo apt-get install e2fslibs-dev' **note: Linux/ext2_fs.h is depreciated
+#include <ext2fs/ext2_fs.h> // install with 'sudo apt-get install e2fslibs-dev' **note: Linux/ext2_fs.h is depreciated
 #include <libgen.h>
 #include <string.h>
 #include <sys/stat.h>
@@ -16,6 +16,7 @@ typedef unsigned char u8;
 typedef unsigned short u16;
 typedef unsigned int u32;
 
+// define shorter TYPES for convenience
 typedef struct ext2_super_block SUPER;
 typedef struct ext2_group_desc GD;
 typedef struct ext2_inode INODE;
@@ -26,41 +27,49 @@ GD *gp;
 INODE *ip;
 DIR *dp;
 
+// Block number of EXT2 FS on FD
 #define SUPERBLOCK 1
 #define GDBLOCK 2
 #define ROOT_INODE 2
 
+// Default dir and regular file modes
 #define DIR_MODE 0x41ED
 #define FILE_MODE 0x81AE
 #define SUPER_MAGIC 0xEF53
 #define SUPER_USER 0
 
+// Proc status
 #define FREE 0
 #define READY 1
 
+// file system table sizes
 #define BLKSIZE 1024
 #define NMINODE 128
 #define NFD 16
 #define NPROC 2
 
+// In-memory inode structure
 typedef struct minode
 {
-  INODE INODE;
-  int dev, ino;
-  int refCount;
-  int dirty;
-  int mounted;
-  struct mntable *mptr;
+  INODE INODE;          // disk Inode
+  int dev;              
+  int ino;              
+  int refCount;         // use count
+  int dirty;            // modified flag
+  int mounted;          // mounted flag
+  struct mntable *mptr; // mount table pointer
 } MINODE;
 
+// Open file table
 typedef struct oft
 {
-  int mode;
-  int refCount;
-  MINODE *mptr;
-  int offset;
+  int mode;     // mode of opened file
+  int refCount; // number of PROCs sharing this instance
+  MINODE *mptr; // pointer to minode of file
+  int offset;   // byte offset for R|W
 } OFT;
 
+// Proc structure
 typedef struct proc
 {
   struct proc *next;
@@ -75,17 +84,17 @@ typedef struct proc
 // Mount Table structure
 typedef struct mtable
 {
-  int dev;
-  int ninodes;
-  int nblocks;
-  int free_blocks;
-  int free_inodes;
-  int bmap;
-  int imap;
-  int iblock;
-  MINODE *mntDirPtr;
-  char devName[64];
-  char mntName[64];
+  int dev;           // device number - 0 for FREE
+  int ninodes;       // from superblock
+  int nblocks;       
+  int free_blocks;   // from superblock and GD
+  int free_inodes;   
+  int bmap;          // from group descriptor
+  int imap;          
+  int iblock;        // inodes start block
+  MINODE *mntDirPtr; // mount point DIR pointer
+  char devName[64];  // device name
+  char mntName[64];  // mount point DIR name
 } MTABLE;
 
 #endif
